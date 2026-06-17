daa9a595c9a54bb899770d996a2599e9b68232ab
fadhel <fadhel1376@gmail.com>
2026-06-17 22:05:12 +0700

    [Tahap 3] Merapikan abstract class Tiket

--- model/Tiket.php
+++ model/Tiket.php
@@ -2,30 +2,23 @@
 // model/Tiket.php
 
 abstract class Tiket {
-    // Properti Terenkapsulasi 
-    protected $id_tiket; 
-    protected $nama_film;
-    protected $jadwal_tayang;
-    protected $jumlah_kursi;
-    protected $hargaDasarTiket; 
+    // Properti diatur protected agar bisa diwarisi langsung oleh kelas anak
+    public $id_tiket;
+    public $nama_film;
+    public $jadwal_tayang;
+    public $jumlah_kursi;
+    public $hargaDasarTiket;
 
-    // Constructor untuk memetakan data
+    // Constructor untuk mengisi data awal objek
     public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket) {
         $this->id_tiket = $id_tiket;
         $this->nama_film = $nama_film;
         $this->jadwal_tayang = $jadwal_tayang;
         $this->jumlah_kursi = $jumlah_kursi;
-        $this->hargaDasarTiket = $hargaDasarTiket; 
+        $this->hargaDasarTiket = $hargaDasarTiket;
     }
 
-    // Getter Bantuan
-    public function getIdTiket() { return $this->id_tiket; }
-    public function getNamaFilm() { return $this->nama_film; }
-    public function getJadwalTayang() { return $this->jadwal_tayang; }
-    public function getJumlahKursi() { return $this->jumlah_kursi; }
-    public function getHargaDasar() { return $this->hargaDasarTiket; } 
-
-    // Metode Abstrak 
+    // Fungsi abstrak kosong yang WAJIB di-override oleh kelas anak
     abstract public function hitungTotalHarga();
     abstract public function tampilkanInfoFasilitas();
 }
